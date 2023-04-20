d10a26fd215d7f3a36dca1361c6a31f96b6b68bf
Maicol Ortiz Hernandez <107804493+maicolortz@users.noreply.github.com>
2023-04-20 18:43:26 -0500

    feat:tailwind added

--- components/Users.jsx
+++ components/Users.jsx
@@ -5,22 +5,28 @@ import { useRouter } from "next/navigation";
 function Users({ users }) {
   const router = useRouter();
   return (
-    <ul className="list-group">
+    <ul className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8 gap-5 ">
       {users?.map((user) => (
         <li
           key={user.id}
-          className="list-group-item d-flex justify-content-between align-items-center"
           onClick={() => {
             router.push(`/users/${user.id}`);
           }}
+          className="card lg:card-side bg-base-100 shadow-xl bg-slate-600 my-2 hover:translate-y-1 hover:transition-colors hover:shadow-lg"
         >
-          <div>
-            <h5>
-              {user.id} {user.first_name} {user.last_name}{" "}
-            </h5>
+          <figure>
+            <img src={user.avatar} alt="Album" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">
+              {" "}
+              {user.first_name} {user.last_name}
+            </h2>
             <p>{user.email}</p>
+            <div className="card-actions justify-end">
+              {/* <button className="btn btn-primary">ver</button> */}
+            </div>
           </div>
-          <img src={user.avatar} alt="" />
         </li>
       ))}
     </ul>
